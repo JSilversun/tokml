c512054f7fded90d6bd299dcad58f21fe9f69ab8
Julia <js@plusteam.tech>
2019-05-11 15:05:31 -0400

    ⛄️ Implement option excludeStyleProperties - The option excludeStyleProperties allows you to exclude the styles properties from the extended data in order to make the kml smaller - Implement `rootProperties` to use properties in the root directory of each `Placemark`, instead of the Extended Data option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var strxml = require('strxml'),
     tag = strxml.tag,
     encode = strxml.encode;
+const {pickBy} = require('lodash');
 
 module.exports = function tokml(geojson, options) {
 
@@ -10,7 +11,8 @@ module.exports = function tokml(geojson, options) {
         name: 'name',
         description: 'description',
         simplestyle: false,
-        timestamp: 'timestamp'
+        timestamp: 'timestamp',
+        excludeStyleProperties: false
     };
 
     return '<?xml version="1.0" encoding="UTF-8"?>' +
@@ -19,15 +21,15 @@ module.exports = function tokml(geojson, options) {
                 documentName(options) +
                 documentDescription(options) +
                 root(geojson, options)
-               ), [['xmlns', 'http://www.opengis.net/kml/2.2']]);
+            ), [['xmlns', 'http://www.opengis.net/kml/2.2']]);
 };
 
 function feature(options, styleHashesArray) {
-    return function(_) {
+    return function (_) {
         if (!_.properties || !geometry.valid(_.geometry)) return '';
         var geometryString = geometry.any(_.geometry);
         if (!geometryString) return '';
-        
+
         var styleDefinition = '',
             styleReference = '';
         if (options.simplestyle) {
@@ -39,7 +41,7 @@ function feature(options, styleHashesArray) {
                         styleHashesArray.push(styleHash);
                     }
                     styleReference = tag('styleUrl', '#' + styleHash);
-                } else if ((geometry.isPolygon(_.geometry) || geometry.isLine(_.geometry)) && 
+                } else if ((geometry.isPolygon(_.geometry) || geometry.isLine(_.geometry)) &&
                     hasPolygonAndLineStyle(_.properties)) {
                     if (styleHashesArray.indexOf(styleHash) === -1) {
                         styleDefinition = polygonAndLineStyle(_.properties, styleHash);
@@ -50,11 +52,12 @@ function feature(options, styleHashesArray) {
                 // Note that style of GeometryCollection / MultiGeometry is not supported
             }
         }
-        
+
         return styleDefinition + tag('Placemark',
+            proccessRootProperties(_.rootProperties) +
             name(_.properties, options) +
             description(_.properties, options) +
-            extendeddata(_.properties) +
+            extendeddata(options.excludeStyleProperties ? pickBy(_.properties, excludeStyleProperties) : _.properties) +
             timestamp(_.properties, options) +
             geometryString +
             styleReference);
@@ -64,7 +67,7 @@ function feature(options, styleHashesArray) {
 function root(_, options) {
     if (!_.type) return '';
     var styleHashesArray = [];
-            
+
     switch (_.type) {
         case 'FeatureCollection':
             if (!_.features) return '';
@@ -100,77 +103,88 @@ function timestamp(_, options) {
     return _[options.timestamp] ? tag('TimeStamp', tag('when', encode(_[options.timestamp]))) : '';
 }
 
+function excludeStyleProperties(property, key) {
+    const styles = ['stroke', 'stroke-width', 'stroke-opacity', 'fill', 'fill-opacity'];
+    return !styles.includes(key);
+}
+
+function proccessRootProperties(_) {
+    return Object.entries.map(([key, value]) => tag(key, encode(value)))
+}
+
 // ## Geometry Types
 //
 // https://developers.google.com/kml/documentation/kmlreference#geometry
 var geometry = {
-    Point: function(_) {
+    Point: function (_) {
         return tag('Point', tag('coordinates', _.coordinates.join(',')));
     },
-    LineString: function(_) {
+    LineString: function (_) {
         return tag('LineString', tag('coordinates', linearring(_.coordinates)));
     },
-    Polygon: function(_) {
+    Polygon: function (_) {
         if (!_.coordinates.length) return '';
         var outer = _.coordinates[0],
             inner = _.coordinates.slice(1),
             outerRing = tag('outerBoundaryIs',
                 tag('LinearRing', tag('coordinates', linearring(outer)))),
-            innerRings = inner.map(function(i) {
+            innerRings = inner.map(function (i) {
                 return tag('innerBoundaryIs',
                     tag('LinearRing', tag('coordinates', linearring(i))));
             }).join('');
         return tag('Polygon', outerRing + innerRings);
     },
-    MultiPoint: function(_) {
+    MultiPoint: function (_) {
         if (!_.coordinates.length) return '';
-        return tag('MultiGeometry', _.coordinates.map(function(c) {
-            return geometry.Point({ coordinates: c });
+        return tag('MultiGeometry', _.coordinates.map(function (c) {
+            return geometry.Point({coordinates: c});
         }).join(''));
     },
-    MultiPolygon: function(_) {
+    MultiPolygon: function (_) {
         if (!_.coordinates.length) return '';
-        return tag('MultiGeometry', _.coordinates.map(function(c) {
-            return geometry.Polygon({ coordinates: c });
+        return tag('MultiGeometry', _.coordinates.map(function (c) {
+            return geometry.Polygon({coordinates: c});
         }).join(''));
     },
-    MultiLineString: function(_) {
+    MultiLineString: function (_) {
         if (!_.coordinates.length) return '';
-        return tag('MultiGeometry', _.coordinates.map(function(c) {
-            return geometry.LineString({ coordinates: c });
+        return tag('MultiGeometry', _.coordinates.map(function (c) {
+            return geometry.LineString({coordinates: c});
         }).join(''));
     },
-    GeometryCollection: function(_) {
+    GeometryCollection: function (_) {
         return tag('MultiGeometry',
             _.geometries.map(geometry.any).join(''));
     },
-    valid: function(_) {
+    valid: function (_) {
         return _ && _.type && (_.coordinates ||
             _.type === 'GeometryCollection' && _.geometries && _.geometries.every(geometry.valid));
     },
-    any: function(_) {
+    any: function (_) {
         if (geometry[_.type]) {
             return geometry[_.type](_);
         } else {
             return '';
         }
     },
-    isPoint: function(_) {
+    isPoint: function (_) {
         return _.type === 'Point' ||
-        _.type === 'MultiPoint';
+            _.type === 'MultiPoint';
     },
-    isPolygon: function(_) {
+    isPolygon: function (_) {
         return _.type === 'Polygon' ||
-        _.type === 'MultiPolygon';
+            _.type === 'MultiPolygon';
     },
-    isLine: function(_) {
+    isLine: function (_) {
         return _.type === 'LineString' ||
-        _.type === 'MultiLineString';
+            _.type === 'MultiLineString';
     }
 };
 
 function linearring(_) {
-    return _.map(function(cds) { return cds.join(','); }).join(' ');
+    return _.map(function (cds) {
+        return cds.join(',');
+    }).join(' ');
 }
 
 // ## Data
@@ -231,22 +245,22 @@ function polygonAndLineStyle(_, styleHash) {
         tag('color', hexToKmlColor(_['stroke'], _['stroke-opacity']) || 'ff555555') +
         tag('width', _['stroke-width'] === undefined ? 2 : _['stroke-width'])
     ]);
-    
+
     var polyStyle = '';
-    
+
     if (_['fill'] || _['fill-opacity']) {
         polyStyle = tag('PolyStyle', [
             tag('color', hexToKmlColor(_['fill'], _['fill-opacity']) || '88555555')
         ]);
     }
-    
+
     return tag('Style', lineStyle + polyStyle, [['id', styleHash]]);
 }
 
 // ## Style helpers
 function hashStyle(_) {
     var hash = '';
-    
+
     if (_['marker-symbol']) hash = hash + 'ms' + _['marker-symbol'];
     if (_['marker-color']) hash = hash + 'mc' + _['marker-color'].replace('#', '');
     if (_['marker-size']) hash = hash + 'ms' + _['marker-size'];
@@ -255,34 +269,34 @@ function hashStyle(_) {
     if (_['stroke-opacity']) hash = hash + 'mo' + _['stroke-opacity'].toString().replace('.', '');
     if (_['fill']) hash = hash + 'f' + _['fill'].replace('#', '');
     if (_['fill-opacity']) hash = hash + 'fo' + _['fill-opacity'].toString().replace('.', '');
-    
+
     return hash;
 }
 
 function hexToKmlColor(hexColor, opacity) {
     if (typeof hexColor !== 'string') return '';
-    
+
     hexColor = hexColor.replace('#', '').toLowerCase();
-    
+
     if (hexColor.length === 3) {
-        hexColor = hexColor[0] + hexColor[0] + 
-        hexColor[1] + hexColor[1] + 
-        hexColor[2] + hexColor[2];
+        hexColor = hexColor[0] + hexColor[0] +
+            hexColor[1] + hexColor[1] +
+            hexColor[2] + hexColor[2];
     } else if (hexColor.length !== 6) {
         return '';
     }
-    
+
     var r = hexColor[0] + hexColor[1];
     var g = hexColor[2] + hexColor[3];
     var b = hexColor[4] + hexColor[5];
-    
+
     var o = 'ff';
     if (typeof opacity === 'number' && opacity >= 0.0 && opacity <= 1.0) {
         o = (opacity * 255).toString(16);
         if (o.indexOf('.') > -1) o = o.substr(0, o.indexOf('.'));
         if (o.length < 2) o = '0' + o;
     }
-    
+
     return o + b + g + r;
 }
 
